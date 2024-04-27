665b162a692a190cbe19db49555250d00554725e
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-04-28 01:29:06 +0800

    init

--- src/components/dwWelcome.vue
+++ src/components/dwWelcome.vue
@@ -4,9 +4,7 @@
 
 <template>
     <img class="logo" src="/images/icon-512.png" />
-    <h1 class="appName">追 劇 小 幫 手
-
-    </h1>
+    <h1 class="appName">追 劇 小 幫 手</h1>
     <small>
         <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 8 8">
             <path fill="currentColor" d="M0 0v3l5 5l3-3l-5-5zm2 1c.55 0 1 .45 1 1s-.45 1-1 1s-1-.45-1-1s.45-1 1-1" />
@@ -23,8 +21,8 @@
 }
 
 .logo {
-    margin-top: 20px;
-    max-width: 50%;
+    margin-top: 80px;
+    max-width: 60%;
     border-radius: 20px;
     filter: drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.7))
 }
