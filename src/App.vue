1802a48f1b293a44b6b8c3fbad767d33894b647e
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-04-27 17:56:32 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,37 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+import {ref} from "@vue/reactivity"
+
+const section = ref("demo")
+const setSection = (value) => {
+  section.value = value;
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="demoMenu">
+    <button @click="setSection('test')">TEST</button>
+    <button @click="setSection('demo')">DEMO</button>
+  </div>
+  <div id="test" v-if="section === 'test'">
+    <h1>測試成功！</h1>
+  </div>
+  <div id="demo" v-if="section === 'demo'">
+    <header>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <main>
+      <TheWelcome />
+    </main>
+  </div>
 </template>
 
 <style scoped>
