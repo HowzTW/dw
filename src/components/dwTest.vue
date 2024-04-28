d138e9b9197b5675b9b2ca5a240c748d12c60540
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-04-28 20:41:20 +0800

    init

--- src/components/dwTest.vue
+++ src/components/dwTest.vue
@@ -1,18 +1,108 @@
 <script setup>
+/*
+import axios from 'axios';
+
+function getWebsiteData(){
+    let self = this;
+    let url = 'https://www.msn.com/en-au?AR=1'; // url we get data from
+    let dataArray = [];                         // we put data in this array
+    // GET request for remote image in node.js
+    axios({
+        method: 'get',
+        url: url,
+    })
+        .then(function (response) {
+            let html = response.data;
+
+            let $ = cheerio.load(html);
+
+            $("ul.tertiary li").each(function () {
+                const title = $(this).find('h3').attr('aria-label');
+                const image = $(this).find('img').attr('src');
+
+                // putting data in array.
+
+                dataArray.push({
+                    'title': title,
+                    'image': image
+                })
+            });
+
+            self.lastestArticles = dataArray;     // Here we assign value to vuejs variable
+
+            console.log(dataArray);
+        });
+}
+
+getWebsiteData();
+*/ 
+
+/*
+const cors = 'https://cors-anywhere.herokuapp.com/'; // use cors-anywhere to fetch api data
+const url = 'https://www.msn.com/zh-tw'; // origin api url
+
+//const htmlMsg = ref("");
+
+// fetch api url by cors-anywhere 
+axios.get(`${cors}${url}`)
+  .then((response) => {
+    const msg = response.data;
+    //htmlMsg.value = JSON.stringify(msg)
+  })
+  .catch(function (error) { 
+   
+   console.log(error);
+ });
+
+*/
+ 
+/*
+ axios({
+    url: 'https://nodejs.org/en/blog/',
+    method: 'get',
+    headers: {
+       'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    data: qs.stringify(postBody)
+})  
+.then((response) => {
+    const msg = response.data;
+    //htmlMsg.value = JSON.stringify(msg)
+  })
+  .catch(function (error) { 
+   
+   console.log(error);
+ });
+
+*/
+import { ref } from "@vue/reactivity"
+import axios from 'axios';
+
+const result = ref("");
+
+axios
+.get('https://nodejs.org/en/blog/')
+.then(
+    response => {
+        console.log("OKKKKKKK");
+        result.value = "OK"
+    }
+)
+.catch(
+    error => {
+        console.log(error);
+        result.value = "NO"
+    }
+)
+
+
 </script>
 
 <template>
-    <div class="div100w divFlex">
-    <h1>這是一個真實的世界</h1>
-        <div class="div50w">
-            左邊50
-        </div>
-        <div class="div50w">
-            右邊50
-        </div>
+    <div>
+        <h1> Web Scraper</h1>
+        <h2>{{ result }}</h2>
     </div>
 </template>
 
-<style scoped>
-
-</style>
+<style></style>
