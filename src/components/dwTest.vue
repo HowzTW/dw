acbbe6a7d2c9710991be47e1ae2950d58bf105a5
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-05-03 18:54:57 +0800

    init

--- src/components/dwTest.vue
+++ src/components/dwTest.vue
@@ -1,31 +1,41 @@
+<script setup>
+import { ref } from "@vue/reactivity"
+
+const filename = ref("/data/drama/gimy.ai.258606.json")
+var jsonObj = ref('(blank)')
+
+const getJsonData = () => {
+    fetch(filename.value)
+    .then((r) => r.json())
+    .then(
+        (json) => {
+            jsonObj.value = json;
+            console.log(jsonObj.value);
+        },
+        (response) => {
+            console.log('Error loading json:', response)
+        }
+    ); //then
+} 
+
+</script>
+
 <template>
     <div>
-      <input type="text" v-model="filename" placeholder="Enter filename">
-      <textarea v-model="datastr" placeholder="Enter data"></textarea>
-      <button @click="saveToFile">Save to File</button>
+        <h1>JSON讀寫測試</h1>
+        <p>
+            請輸入檔案名稱：<br />
+            <input type="text" v-model="filename" placeholder="請輸入檔案名稱" /><br />
+            <button @click="getJsonData">讀取</button><br />
+            <textarea> {{ jsonObj }}</textarea><br />
+        </p>
+        <p v-for="filmSource in jsonObj.filmSources">
+            {{ filmSource.filmSourceId }}
+            <textarea v-for="episode in filmSource.episodes"> {{ episode.episodeId }}</textarea>
+        </p>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        filename: '',
-        datastr: ''
-      };
-    },
-    methods: {
-      saveToFile() {
-        if (this.filename && this.datastr) {
-          const blob = new Blob([this.datastr], { type: 'text/plain' });
-          const link = document.createElement('a');
-          link.href = window.URL.createObjectURL(blob);
-          link.download = this.filename;
-          link.click();
-        } else {
-          alert('Please enter filename and data');
-        }
-      }
-    }
-  }
-  </script>
+</template>
+
+<style scoped>
+
+</style>
