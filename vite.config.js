1a63fbd536679fc0a06e496e4f2ec0096c7726b2
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-05-05 00:10:58 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -66,7 +66,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '/data': '/data'
+      '/data': 'data'
     }
   }
 })
