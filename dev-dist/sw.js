d5c86381555b1542f3ca07c1d8a1d8b823c54839
HowzTW <34091465+HowzTW@users.noreply.github.com>
2024-05-11 22:34:27 +0800

    init

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qci8i15unk"
+    "revision": "0.d2d91hau3d"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
